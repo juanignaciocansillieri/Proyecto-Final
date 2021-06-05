32b1f67572a0ba9a290283889cbaa8e5ca53d085
juancansi <juancansi@gmail.comgit config --global user.name Your>
2021-06-05 19:51:08 -0300

    agregue funciones sql y comentarios

--- index.py
+++ index.py
@@ -4,21 +4,29 @@ from pymysql import connect, err
 from pymysql.err import Error
 print("hola")
 
-PRODUCTS_TABLE = """CREATE TABLE produc (
-        id int UNSIGNED AUTO_INCREMENT PRIMARY KEY,  
-        nombre VARCHAR(10),
+#ELIMINAR TABLA
+DROP_TABLE = """DROP TABLE IF EXISTS produc     """ 
+#CREAR TABLA
+PRODUCTS_TABLE = """CREATE TABLE produc (  
+        id int UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,  
+        nombre VARCHAR(10) NOT NULL,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);"""
 
 if __name__ == '__main__':
         try:
-                connect = pymysql.connect(host="localHOST",port=3306,user="root",password="asd123",database="prueba1")
+                connect = pymysql.connect(host="localhost",port=3306,user="root",password="asd123",database="prueba1")
                 print("exitosamente")
                 cursor = connect.cursor()       
+                cursor.execute(DROP_TABLE)
                 cursor.execute(PRODUCTS_TABLE)
         except pymysql.err.OperationalError as err:
                 print("hubo un error:" ,err)
 
-                
+        finally:
+                cursor.close()
+                connect.close()
+                print("se cerro la conexión")
+                #SIEMPRE CERRAR CUANDO NO SE LO UTILIZA MÁS, ASI LIBERA PROCESOS.
 
 
 lista = ["h","o","l","a"]
