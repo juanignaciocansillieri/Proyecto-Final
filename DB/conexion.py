37c315f171116c09038046161c30455361e5bfb3
alextexas <jolanotei@gmail.com>
2021-06-08 00:11:11 -0300

    .

--- DB/conexion.py
+++ DB/conexion.py
@@ -28,7 +28,7 @@ def close_connection(con):  #cierra conexion a db
         print("Hubo un error:",err)
 
 def borrar_tabla(con):  #borra tablas (posible modificacion futura: ingresar el nombre de la tabla y que la borre)
-    q="DROP TABLE IF EXISTS produc;" #se usa "," para mas de una
+    q="DROP TABLE IF EXISTS productos, usuarios,alojamiento;" #se usa "," para mas de una
     try:
         cur=con.cursor()
         cur.execute(q)
@@ -38,19 +38,48 @@ def borrar_tabla(con):  #borra tablas (posible modificacion futura: ingresar el 
         print("Hubo un error:",err)
 
 def crear_tabla(con):   #crea una tabla (al iniciar por primera vez el programa se crearan todas)
-    q="CREATE TABLE IF NOT EXISTS produc(id int UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL, nombre VARCHAR(30) NOT NULL,apellido VARCHAR(30) NOT NULL,edad int(2) NOT NULL,created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);"
+    q="""CREATE TABLE IF NOT EXISTS usuarios (
+  idusuarios INT UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  nombre VARCHAR(20) NOT NULL,
+  apellido VARCHAR(20) NOT NULL,
+  tipo BINARY(1) NOT NULL,
+  puesto VARCHAR(20) NOT NULL,
+  nacimiento DATE NOT NULL);"""
+    q2="""CREATE TABLE IF NOT EXISTS productos (
+  idproductos INT UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  codigo VARCHAR(20) NOT NULL,
+  nombre VARCHAR(20) NOT NULL,
+  marca VARCHAR(20) NOT NULL,
+  cantidad INT NOT NULL,
+  descripcion VARCHAR(50) NOT NULL,
+  ubicacion VARCHAR(20) NOT NULL,
+  foto VARCHAR(45) NOT NULL,
+  lote VARCHAR(20) NOT NULL,
+  vencimiento DATE NOT NULL,
+  refrigeracion BINARY(1) NOT NULL,
+  inflamabre BINARY(1) NOT NULL,
+  fragil BINARY(1) NOT NULL);"""
+    q3="""CREATE TABLE IF NOT EXISTS alojamiento (
+  idalojamiento INT UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  dimensiones VARCHAR(20) NOT NULL,
+  disponibilidad BINARY(1) NOT NULL,
+  posicion VARCHAR(20) NOT NULL,
+  refrigeracion BINARY(1) NOT NULL,
+  limite VARCHAR(20) NOT NULL);"""
     try:
         cur=con.cursor()
         cur.execute(q)
+        cur.execute(q2)
+        cur.execute(q3)
         cur.close()
         print("se creo con exito")
     except pymysql.err.OperationalError as err:
         print("Hubo un error:",err)
 
-def contar_filas_tabla(con):
+def contar_filas_tabla(con):    #despues hay que poner de que tabla queremos contar
     #cuenta las filas de una tabla especifica
     #(modificacion futura: definir la sentencia de la funcion para que cuente en la tabal donde sea necesario)
-    q="SELECT COUNT(*) from produc;"
+    q="SELECT COUNT(*) from productos;"
     try:
         cur=con.cursor()
         cur.execute(q)
@@ -60,3 +89,4 @@ def contar_filas_tabla(con):
         print("hay ",a,"vistas")
     except pymysql.err.OperationalError as err:
         print("Hubo un error:",err)
+        #despues tiene que hacer un return
--- pruebas2.py
+++ pruebas2.py
@@ -4,6 +4,6 @@ import pruebas as p
 from DB import conexion as c
 a=c.start_connection()
 c.borrar_tabla(a)
-#c.crear_tabla(a)
+c.crear_tabla(a)
 #c.contar_filas_tabla(a)
 c.close_connection(a)
