52dae39327f2062ee35b0ac6f84d6e0a1cfc021e
juancansi <juancansi@gmail.comgit config --global user.name Your>
2021-07-26 01:56:05 -0300

    asds

--- Interfaces/login/splash_func.py
+++ Interfaces/login/splash_func.py
@@ -1,3 +1,4 @@
+from DB import index as i
 import sys
 from PyQt5 import QtWidgets
 from PyQt5 import QtGui
@@ -7,10 +8,42 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 import splash
 import Img.img
+from login_funcional import Ui_MainWindow
+import login_funcional as l
+sys.path.append("..")
+
 counter = 0
+
+
+class Login(QMainWindow):
+
+    def __init__(self):
+        super(QMainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        print(Ui_MainWindow)
+        self.ui.setupUi(self)
+        self.setWindowFlag(Qt.FramelessWindowHint)
+        self.setAttribute(Qt.WA_TranslucentBackground)
+        self.login = l.LoginWindow()
+        self.btnLogin = self.ui.login_btn
+        self.btnLogin.clicked.connect(self.verificacion)
+
+    def verificacion(self):
+        user = self.ui.user_login_input.text()
+        password = self.ui.pass_login_input.text()
+        usuario = i.verificar_usuario(user, password)
+        if usuario == 1:
+            print("cargado exitosamente")
+        else:
+            print("hola")
+            QtWidgets.QMessageBox.critical(self, "Error", "Datos incorrectos")
+            self.ui.user_login_input.setText("")
+            self.ui.pass_login_input.setText("")
+
 
 class Splash(QMainWindow):
     a = 0
+
     def __init__(self):
         super(Splash, self).__init__()
         self.ui = splash.Ui_MainWindow()
@@ -23,62 +56,61 @@ class Splash(QMainWindow):
         self.shadow.setBlurRadius(20)
         self.shadow.setXOffset(0)
         self.shadow.setYOffset(0)
-        self.shadow.setColor(QColor(0,0,0,60))
+        self.shadow.setColor(QColor(0, 0, 0, 60))
         ###### INSERTAR CÍRCULO DE PROGRESO##############
-        self.circle = CircularProgress()        
+        self.circle = CircularProgress()
         self.ui.horizontalLayout_3.addWidget(self.circle)
         ###### CERRAR SPLASH #############
         print(self.circle.timer.duration)
         self.timer = QtCore.QTimer()
-        self.time = QtCore.QTime(0,0,0)
-        
+        self.time = QtCore.QTime(0, 0, 0)
+
         def timerEvent():
             global time
             self.time = self.time.addSecs(1)
-            print(str(self.time))
             if str(self.time) == "PyQt5.QtCore.QTime(0, 0, 7)":
-                print("hola")
                 self.close()
-            
+                self.login = Login()
+                self.login.show()
+
         self.timer.timeout.connect(timerEvent)
         self.timer.start(1000)
 
-           
 
 class CircularProgress(QWidget):
     counter = 0
     b = 0
     a = 0
+
     def __init__(self):
-        super().__init__()  
-                #Global
-        self.contador =str(0)
-                 # Record angle
-        self.angle=0
-                 # This is for drawing, angle is the real angle
-        self.drawAngle=self.angle
-                 # Progress bar width
-        self.lineWidth=18
-                 # Timeline for animation
-                #timer
-        self.timer = QTimeLine(7000,self)
-        self.timer.setFrameRange(0,360)
+        super().__init__()
+        # Global
+        self.contador = str(0)
+        # Record angle
+        self.angle = 0
+        # This is for drawing, angle is the real angle
+        self.drawAngle = self.angle
+        # Progress bar width
+        self.lineWidth = 18
+        # Timeline for animation
+        # timer
+        self.timer = QTimeLine(7000, self)
+        self.timer.setFrameRange(0, 360)
         self.timer.frameChanged.connect(self.progress)
         self.timer.start()
         print(self.drawAngle)
 
         ######## CREAR LABEL CONTADOR ##########
         self.label = QtWidgets.QLabel(self)
-        self.label.move(287,123)
-        self.label.resize(110,50)
-        self.label.setStyleSheet("font-family: roboto;\n"
-"font-weight: lighter;\n"
-"color:#FFF;\n"
-"font-size: 25px;\n"
-"\n"
-"background:none")
+        self.label.move(287, 123)
+        self.label.resize(110, 50)
+        self.label.setStyleSheet("font-family: roboto;\n""font-weight: lighter;\n"
+                                 "color:#FFF;\n"
+                                 "font-size: 25px;\n"
+                                 "\n"
+                                 "background:none")
 
-    def progress(self,frame):
+    def progress(self, frame):
         global counter
         global b
         self.angle = self.counter
@@ -94,34 +126,35 @@ class CircularProgress(QWidget):
         else:
             self.label.setText("100%")
             self.b = 1
-   
-    
-    def paintEvent(self,event):
+
+    def paintEvent(self, event):
         if self.a == 0:
-                 # There is a problem here, pyqt5 cannot implicitly convert QRect to QRectF (PySide2 can), so use QRectF directly
-            the_rect=QRectF(200,50,200,200)
-                 # Paintbrush
-            painter=QPainter(self)
-            painter.setRenderHints(QPainter.Antialiasing|QPainter.SmoothPixmapTransform,on=True)
-                 # Mirror flip, so it is clockwise
-            painter.setViewport(self.width(),0,-self.width(),self.height())
- 
-                 # path default OddEvenFill, this fills the intersecting part of the two circles
-            the_path=QPainterPath()
-            the_path.addEllipse(the_rect.adjusted(1,1,-1,-1))
-            the_path.addEllipse(the_rect.adjusted( 
-                1+self.lineWidth,1+self.lineWidth,-1-self.lineWidth,-1-self.lineWidth))
-             #color inicial
-            painter.fillPath(the_path,QColor("#12151a"))
- 
-                 # Radial gradient (parameters are the center point and the starting angle), by default it is calculated counterclockwise from the right
-            the_gradient=QConicalGradient(the_rect.center(),90)
-            the_angle=self.drawAngle/360
-            the_gradient.setColorAt(0,QColor(17, 115, 255, 255))
-            the_gradient.setColorAt(the_angle,QColor(17, 115, 255, 109))
-            if the_angle+0.001<1:
-                the_gradient.setColorAt(the_angle+0.001,QColor(0,0,0,0))
-                painter.fillPath(the_path,the_gradient)
+            # There is a problem here, pyqt5 cannot implicitly convert QRect to QRectF (PySide2 can), so use QRectF directly
+            the_rect = QRectF(200, 50, 200, 200)
+            # Paintbrush
+            painter = QPainter(self)
+            painter.setRenderHints(
+                QPainter.Antialiasing | QPainter.SmoothPixmapTransform, on=True)
+            # Mirror flip, so it is clockwise
+            painter.setViewport(self.width(), 0, -self.width(), self.height())
+
+            # path default OddEvenFill, this fills the intersecting part of the two circles
+            the_path = QPainterPath()
+            the_path.addEllipse(the_rect.adjusted(1, 1, -1, -1))
+            the_path.addEllipse(the_rect.adjusted(
+                1+self.lineWidth, 1+self.lineWidth, -1-self.lineWidth, -1-self.lineWidth))
+            # color inicial
+            painter.fillPath(the_path, QColor("#12151a"))
+
+            # Radial gradient (parameters are the center point and the starting angle), by default it is calculated counterclockwise from the right
+            the_gradient = QConicalGradient(the_rect.center(), 90)
+            the_angle = self.drawAngle/360
+            the_gradient.setColorAt(0, QColor(17, 115, 255, 255))
+            the_gradient.setColorAt(the_angle, QColor(17, 115, 255, 109))
+            if the_angle+0.001 < 1:
+                the_gradient.setColorAt(the_angle+0.001, QColor(0, 0, 0, 0))
+                painter.fillPath(the_path, the_gradient)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
