3bee35d951fb0a09dad5b53808576175067ce4a0
nicolasrija <nicolasajir@gmail.com>
2021-07-29 21:17:27 -0300

    Cargué main, icon y archivos UI

---
+++ ui_functions.py
@@ -0,0 +1,25 @@
+from PyQt5.sip import enableoverflowchecking
+from main import *
+
+class UIFunctions(MainWindow):
+    
+    def toggleMenu(self, maxWidth, enable):
+        if enable:
+
+            #GET WIDTH 
+            width = self.ui.frame_left_menu.width()
+            maxExtend = maxWidth
+            standard = 70 
+
+            #SET MAX WIDTH
+            if width == 70:
+                widthExtended = maxExtend
+            else:
+                widthExtended = standard
+
+            #ANIMACIÓN
+        self.animation = QPropertyAnimation(self.ui.frame_left_menu, b"minimumWidth")
+        self.animation.setDuration(400)
+        self.animation.setStartValue(width)
+        self.animation.setEndValue(widthExtended)
+        self.animation.start()
