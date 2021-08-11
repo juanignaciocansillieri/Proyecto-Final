962d75189b82178f8876c8f1bc4dd8efd80ce111
juancansi <juancansi@gmail.comgit config --global user.name Your>
2021-08-11 01:03:20 -0300

    Agregando opcion ninguna a agre.prod

---
+++ Interfaces/main/create_user_func.py
@@ -0,0 +1,29 @@
+import sys
+import os
+from PyQt5 import QtWidgets
+from PyQt5 import QtGui
+from PyQt5 import QtCore
+import PyQt5
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+import create_user
+from create_user import Ui_MainWindow
+
+class UsuarioWindow(QMainWindow):
+
+    def __init__(self):
+        super(UsuarioWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        ############# RECIBIMOS PROPORCIONES DE LA PANTALLA ###########
+        qtRectangle = self.frameGeometry()
+        centerPoint = QDesktopWidget().availableGeometry().center()
+        qtRectangle.moveCenter(centerPoint)
+        self.move(qtRectangle.topLeft())
+        ############## CENTRAMOS LA VENTANA #############
+        centerPoint = QDesktopWidget().availableGeometry().center()
+        qtRectangle.moveCenter(centerPoint)
+        self.move(qtRectangle.topLeft())
+
+        #self.ui.pushButton.clicked.connect(self.crearProducto)
